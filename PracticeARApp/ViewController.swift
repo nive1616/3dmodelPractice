b41d02bfff59881007e649738a7163a69ed53e5e
nive1616 <90475688+nive1616@users.noreply.github.com>
2021-12-13 18:04:12 -0500

    Add files via upload

---
+++ PracticeARApp/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  PracticeARApp
+//
+//  Created by Harsha Karanth on 9/27/21.
+//
+
+import UIKit
+import RealityKit
+import Combine
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var cancellable: AnyCancellable? = nil;
+        let sphereAnchor = AnchorEntity(world: SIMD3(x:0, y:0, z:-50));
+        cancellable = ModelEntity.loadModelAsync(named: "LemonMeringuePie", in:nil).collect().sink(receiveCompletion: {error in print("Error: \(error)")
+            cancellable?.cancel()
+        }, receiveValue: { entities in
+            //var object : ModelEntity = entity
+            for entity in entities {
+                entity.setScale(SIMD3<Float>(0.75,0.75,0.75), relativeTo: sphereAnchor)
+                entity.generateCollisionShapes(recursive: true)
+                sphereAnchor.addChild(entity);
+            }
+            
+        });
+        
+        arView.scene.addAnchor(sphereAnchor);
+    }
+}
